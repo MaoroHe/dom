af9aba0a676b1e7ddc37af68348e74f18019997c
MaoroHe <maorohecq1409@gmail.com>
2023-12-12 16:41:49 +0100

    update

--- 4. navigate/script.js
+++ 4. navigate/script.js
@@ -1,2 +1,18 @@
-/*
-*/
+let olSel = document.querySelector('ol');
+const lChild = olSel.lastElementChild;
+
+olSel.prepend(lChild);
+
+// 2.
+
+let section = document.querySelector('main');
+const a = section.children[1];
+const b = section.children[2];
+let aH2 = a.firstElementChild;
+let bH2 = b.firstElementChild;
+
+section.insertBefore(bH2, a)
+
+// 3.
+
+section.removeChild(a);
